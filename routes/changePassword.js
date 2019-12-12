1d564e40648c681b91aba16b082e5a5af45202b6
Han <allen0346@gmail.com>
2019-12-12 10:56:26 +0800

    Adjust: modification based on the updated passwordValidation func in userService.js

--- routes/changePassword.js
+++ routes/changePassword.js
@@ -1,11 +1,11 @@
 const { validation } = require('../utils/accessTokens');
 const { error, success } = require('../utils/responses');
-const { hashPassword, passwordValidation, updatePassword } = require('../utils/userServices');
+const { passwordValidation, updatePassword } = require('../utils/userServices');
 const _get = require('lodash/get');
 
 const _ = {
   get: _get
-}
+};
 
 const changePassword = (app) => {
   app.post('/user/change-password', async (req, res) => {
@@ -25,37 +25,44 @@ const changePassword = (app) => {
       userId: subid,
       attemptPassword: originalPassword
     });
-    if (!pwValidtionResult) {
-      return error({
+    if (!pwValidtionResult.status === 0) {
+      error({
+        res,
+        status: 400,
+        errorMessage: 'Bad Request. the subId has no match user record or the collection has the doc record but the doc has not been authorized'
+      });
+    } else if (!pwValidtionResult.status === 1) {
+      error({
         res,
         status: 403,
         errorMessage: 'Forbidden. the original password is not correct'
       });
-    }
-    if (!updatedPassword) {
-      return error({
+    } else {
+      if (!updatedPassword) {
+        return error({
+          res,
+          status: 406,
+          errorMessage: 'Not Acceptable. the updated password is empty'
+        });
+      }
+      const updatePasswordResult = updatePassword({
+        userId: subid,
+        password: updatedPassword
+      });
+      if (!updatePasswordResult) {
+        return error({
+          res,
+          status: 500,
+          errorMessage: 'Internel Server Error'
+        });
+      }
+      success({
         res,
-        status: 406,
-        errorMessage: 'Not Acceptable. the updated password is empty'
+        status: 200,
+        data: null,
+        statusCode: 1
       });
     }
-    const updatePasswordResult = updatePassword({
-      userId: subid,
-      password: updatedPassword
-    });
-    if (!updatePasswordResult) {
-      return error({
-        res,
-        status: 500,
-        errorMessage: 'Internel Server Error'
-      });
-    }
-    success({
-      res,
-      status: 200,
-      data: null,
-      statusCode: 1
-    });
   });
 };
 
