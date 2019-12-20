cd79a5269f2680f5655fbabff1dbdf134e4ddb53
Han <allen0346@gmail.com>
2019-12-20 14:27:29 +0800

    Adjust: add nodeEnvIsProd in config

--- config/index.js
+++ config/index.js
@@ -2,6 +2,7 @@ const ENV_CONSTANTS = require('../constants/env');
 
 module.exports = Object.freeze({
   nodeEnv: process.env.NODE_ENV || ENV_CONSTANTS.DEVELOPMENT,
+  nodeEnvIsProd: process.env.NODE_ENV === ENV_CONSTANTS.PRODUCTION,
   origin: process.env.NODE_ENV === ENV_CONSTANTS.PRODUCTION ? 'https://authorization-server.appspot.com' : 'http://localhost:3030',
   allowedOrigins: ['http://localhost:3000', 'http://localhost:8080', 'https://portfolio-and-trial.appspot.com']
 });
