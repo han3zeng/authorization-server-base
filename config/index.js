adebcf9e9c34842418ae4f605f709a1fc116c606
Han <allen0346@gmail.com>
2020-01-31 11:50:16 +0800

    Bug: prod-origin in config

--- config/index.js
+++ config/index.js
@@ -3,6 +3,6 @@ const ENV_CONSTANTS = require('../constants/env');
 module.exports = Object.freeze({
   nodeEnv: process.env.NODE_ENV || ENV_CONSTANTS.DEVELOPMENT,
   nodeEnvIsProd: process.env.NODE_ENV === ENV_CONSTANTS.PRODUCTION,
-  origin: process.env.NODE_ENV === ENV_CONSTANTS.PRODUCTION ? 'https://authorization-server.appspot.com' : 'http://localhost:3030',
+  origin: process.env.NODE_ENV === ENV_CONSTANTS.PRODUCTION ? 'https://authorization-server-261201.appspot.com' : 'http://localhost:3030',
   allowedOrigins: ['http://localhost:3000', 'http://localhost:8080', 'https://portfolio-and-trial.appspot.com']
 });
